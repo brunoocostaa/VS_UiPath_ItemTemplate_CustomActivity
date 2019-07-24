dd91050f4dc182f01bdd36abf8c770496af99298
Bruno Costa <brunoazev.c@gmail.com>
2019-07-23 21:28:48 -0500

    initial commit

---
+++ Class1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace $rootnamespace$
+{
+    /// <summary>
+    /// This custom activity does something.
+    /// </summary>
+    public sealed class $safeitemname$  : CodeActivity
+    {
+        #region .    Activity Name    .
+        //Use this section to define how the activity name will be displayed
+        public $safeitemname$()
+        {
+             this.DisplayName = "$safeitemname$";
+        }
+	    #endregion
+        
+        #region .    UiPath Properties    .
+        
+        [RequiredArgument, Category("Input"), Description("{Your description}")]
+        public InArgument<string> Input
+        {
+            get;
+            set;
+        }
+
+        [Category("Output"), Description("{Your description}")]
+        public OutArgument<string> Output
+        {
+            get;
+            set;
+        }
+
+        #endregion           
+
+        #region .    Methods    .
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            try
+            {                
+                string input = this.Input.Get(context);
+
+                // Your code goes here. ** Do not include many logic here. Use external standard classes instead. **                                
+
+                this.Output.Set(context, string.Empty);
+            }
+            catch (Exception e)
+            {
+                // Your custom message and exception handling goes here.
+                throw e;
+            }
+        }
+
+        #endregion
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,23 @@
+# VS Item Template for UiPath custom activity
+
+Template that provides a quick way to create a C# class (custom activity) on the standards required by UiPath.
+
+--
+
+This VS template makes creating a custom activity for UiPath using Visual Studio a piece of cake! Even if you have never created one and do not know about the specific references that needs to be added (or class modifier, attributes, etc.), you will be able to come up with a custom activity ready to be packaged and deployed to your UiPath project.  
+
+--
+
+Steps to install the template:
+
+1. Download the file CustomActivity.zip
+2. Navigate to VS templates folder
+  E.g.: Documents > Visual Studio 2017 > Templates > ItemTemplates
+3. Move the CustomActivity.zip to the above folder.
+4. Restart Visual Studio.
+5. On your solution, choose Add > New item
+6. The option Custom Activity should be available.
+7. Select a custom activity name as desired and hit Add.
+8. Update the code as needed and publish your class library so you can pack in a nukpg file.
+
+
